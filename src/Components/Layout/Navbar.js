068c5d959ef1d0d95bb45c7305cb99b7a8f54194
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-08-27 07:42:54 +0530

    seperate logout component removed

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LogoutButton from '../Auth/Logout';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
