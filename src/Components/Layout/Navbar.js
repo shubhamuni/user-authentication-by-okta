c9cd898fc0f3c536c7ee9f3e3eda5f959e00fdcb
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-08-27 21:08:42 +0530

    toggling tailwindcss

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
       <div className="container">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/" >
           Acme Staff Portal
         </Link>
         <button
