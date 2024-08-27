4d284345f910de6935f1b6d4dbc2ad31c730f39d
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-08-27 21:23:18 +0530

    bootstrap card modified

--- src/Components/card/Card.js
+++ src/Components/card/Card.js
@@ -5,16 +5,12 @@ function Card(props) {
   const { name, category, shippingInformation, thumbnail } = props;
 
   return (
-    <div className='bg-light-green inline-block rounded-lg p-3 m-3 transform transition duration-200 hover:scale-110 border-2 shadow-lg'>
-      {/* Displaying the product image */}
-      <img alt='product' src={thumbnail} />
-      <div>
-        {/* Displaying the product name */}
-        <h2>{name}</h2>
-        {/* Displaying the product category */}
-        <p>{category}</p>
-        {/* Displaying the product shipping information */}
-        <p>{shippingInformation}</p>
+    <div class="card" style={{width: "18rem"}}>
+      <img src={thumbnail} class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">{name}</h5>
+        <p class="card-text">{category}</p>
+        <p class="card-text">{shippingInformation}</p>
       </div>
     </div>
   );
