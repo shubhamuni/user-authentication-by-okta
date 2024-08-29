757d5c98583179e8df12b5554c417eff5484703d
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-08-29 21:07:03 +0530

    customization

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -3,12 +3,21 @@ import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
-  const { isAuthenticated, logout, user,loginWithRedirect } = useAuth0();
+  const { isAuthenticated, logout, user, loginWithRedirect } = useAuth0();
+  console.log("User", user)
+
+  const capitalizeWords = (string) => {
+    return string
+        .toLowerCase()
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+  }
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
       <div className="container">
         <Link className="navbar-brand" to="/" >
-          Acme Staff Portal
+          Product Analysis 
         </Link>
         <button
           className="navbar-toggler"
@@ -20,19 +29,30 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
+            {isAuthenticated ? <li className="nav-item">
+              <Link className="nav-link" to="/card">
+                Home
+              </Link>
+            </li> :
+              <li>
+              <Link className="nav-link">
                 Home
               </Link>
             </li>
+            }
             {isAuthenticated ?<><li className="nav-item">
               <Link className="nav-link" to="/staff">
                 Staff
               </Link>
             </li>
               <li className="nav-item">
-              <Link className="nav-link" to="/card">
-                  Hello {user.name}
+              <Link className="nav-link">
+                  Welcome 
+              </Link>
+            </li>
+              <li className="nav-item">
+              <Link className="nav-link">
+                  {capitalizeWords(user.name)}
               </Link>
             </li>
             <li className="nav-item">
--- src/Components/card/Card.js
+++ src/Components/card/Card.js
@@ -5,13 +5,13 @@ function Card(props) {
   const { name, email, shippingInformation, thumbnail, price, } = props;
   return (
     <div className="col-md-3 shadow-lg mb-4 p-3 m-5 rounded">
-      <img src={thumbnail} class="card-img-top" alt={name} />
-      <div class="card-body">
-        <h5 class="card-title">{name}</h5>
-        <p class="card-text">{email}</p>
-        <p class="card-text">{email}</p>
-        <p class="card-text">{shippingInformation}</p>
-        <p class="card-text">$ {price}</p>
+      <img src={thumbnail} className="card-img-top" alt={name} />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{email}</p>
+        <p className="card-text">{email}</p>
+        <p className="card-text">{shippingInformation}</p>
+        <p className="card-text">$ {price}</p>
       </div>
     </div>
   );
