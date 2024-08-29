2a106bc43d909431e4118c9402884930a8e3aacb
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-08-29 20:36:08 +0530

    cleared

--- src/Components/card/Card.js
+++ src/Components/card/Card.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Card(props) {
   // Destructuring props to extract relevant data
-  const { name, email, shippingInformation, thumbnail, price } = props;
-  console.log("category:" , email)
+  const { name, email, shippingInformation, thumbnail, price, } = props;
   return (
     <div className="col-md-3 shadow-lg mb-4 p-3 m-5 rounded">
       <img src={thumbnail} class="card-img-top" alt={name} />
       <div class="card-body">
         <h5 class="card-title">{name}</h5>
+        <p class="card-text">{email}</p>
         <p class="card-text">{email}</p>
         <p class="card-text">{shippingInformation}</p>
         <p class="card-text">$ {price}</p>
