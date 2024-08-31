30b514f4eec98d6a7e4ea91a74549dad42e636be
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-08-31 19:26:38 +0530

    border

--- src/Components/card/Card.js
+++ src/Components/card/Card.js
@@ -4,7 +4,7 @@ function Card(props) {
   // Destructuring props to extract relevant data
   const { name, category, shippingInformation, thumbnail, price, } = props;
   return (
-    <div className="col-md-3 shadow-lg mb-4 p-3 m-5 rounded">
+    <div className="col-md-3 shadow-lg mb-4 p-3 m-5 border border-secondary rounded">
       <img src={thumbnail} className="card-img-top" alt={name} />
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
