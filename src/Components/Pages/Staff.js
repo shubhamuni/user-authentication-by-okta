311d601a9d451f466ff8b0da1d2ea10b39af40a1
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-08-31 07:07:42 +0530

    push

--- src/Components/Pages/Staff.js
+++ src/Components/Pages/Staff.js
@@ -1,12 +1,20 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
 
 export default function Staff() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className='container'>
-      <h1>Staff</h1>
-      <div className="alert alert-light" role="alert">
-        Welcome to our staff
-      </div>
+      {isAuthenticated ? (
+        <>
+          <h1>Staff</h1>
+          <div className="alert alert-light" role="alert">
+            Welcome to our staff
+          </div>
+        </>
+      ) :(<div className="alert alert-danger" role="alert">
+            Please login to get access to staff
+          </div>) }
     </div>
   )
 }
